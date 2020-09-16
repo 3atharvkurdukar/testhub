8d6b4342e990b21bf489584f7bb5679f8834cd60
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-09-16 16:49:27 +0530

    Added necessary packages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Amplify, { Analytics } from 'aws-amplify';
+
+import './index.scss';
 import App from './App';
+import config from './aws-exports';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+Amplify.configure(config);
+if (process.env.NODE_ENV === 'development') {
+  Analytics.autoTrack('session', { enable: true });
+  Analytics.autoTrack('pageView', { enable: true });
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
