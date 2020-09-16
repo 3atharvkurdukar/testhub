2d9b83ef4f419ebde1adcd925a8d19d9f484330b
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-09-16 16:51:10 +0530

    Implemented navbar and basic navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar/Navbar';
+import Home from './containers/Home/Home';
+import Error404Page from './containers/Error404Page/Error404Page';
+import ComingSoonPage from './containers/ComingSoonPage/ComingSoonPage';
+
+class App extends Component {
+  render() {
+    let navbarLinks = [
+      { title: 'Home', href: '/' },
+      { title: 'About', href: '/about' },
+      { title: 'Admin', href: '/login/admin' },
+    ];
+
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar links={navbarLinks} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={ComingSoonPage} />
+            <Route component={Error404Page} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+class MyNavbar extends Component {
+  render() {
+    const links = this.props.links.map((link) => (
+      <Nav.Link as={NavLink} key={link.href} to={link.href} exact>
+        {link.title}
+      </Nav.Link>
+    ));
+    let dropdownMenu = null;
+    if (this.props.dropdown) {
+      dropdownMenu = (
+        <NavDropdown title={this.props.dropdown.title}>
+          {this.props.dropdown.items.map((item) => (
+            <NavDropdown.Item as={NavLink} key={item.href} to={item.href} exact>
+              {item.title}
+            </NavDropdown.Item>
+          ))}
+        </NavDropdown>
+      );
+    }
+    return (
+      <Navbar
+        collapseOnSelect
+        variant="light"
+        bg="white"
+        expand="lg"
+        className="px-0 shadow-sm"
+      >
+        <Container>
+          <Navbar.Brand className="text-primary header font-weight-bold">
+            TEST<span className="text-secondary font-weight-normal">HUB</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {links}
+              {dropdownMenu}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+export default MyNavbar;
