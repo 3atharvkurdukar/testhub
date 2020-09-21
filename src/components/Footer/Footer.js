4b9dad472c2539a1574e51ea9a2efde0cfda7aae
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2020-09-21 17:29:49 +0530

    Updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark d-print-none">
-      <Container className="text-center text-light">
-        Powered by{' '}
-        <a
-          href="https://preview.printopark.com/"
-          rel="noopener noreferrer"
-          target="_blank"
-          className="text-light text-decoration-none"
+      <Container className="d-flex justify-content-between text-center text-light">
+        <span>
+          Created by{' '}
+          <a
+            href="https://www.github.com/3atharvkurdukar"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="text-light text-decoration-none"
+          >
+            Atharv Kurdukar
+          </a>
+        </span>
+        <Button
+          as={Link}
+          to="/about"
+          variant="outline-light"
+          size="sm"
+          className="rounded-pill px-3 mx-3"
         >
-          Print-O-Park, Latur
-        </a>
+          SUPPORT THE CAUSE
+        </Button>
       </Container>
     </footer>
   );
